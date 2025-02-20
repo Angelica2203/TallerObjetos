90f63890e21cc57c2aff31481c961e2c561a064b
Sala K 402 <salak402@salasfr.itm.edu.co>
2025-02-20 07:08:44 -0500

    cambio de rama

--- principal.java
+++ principal.java
@@ -8,58 +8,59 @@ public class principal
         Metodos m = new Metodos();
         Scanner sc = new Scanner(System.in);
         int opt = 0;
-        System.out.println("Ingrese el punto que requiere visualizar del 1 al 10: ");
-        while (!sc.hasNextInt()) //negar la respuestas 
-        {
-            System.out.println("dato incorrecto");
-            sc.next();     
-        }
-        opt = sc.nextInt();
-        int d = m.SolicitarDimension();
-        switch (opt) 
-        {
-            case 1:
-                p1 punto1 = new p1();
-                punto1.punto1(d);
-                break;
-            case 2:
-                p2 punto2 = new p2();
-                punto2.punto2(d);
-                break;
-            case 3:
-                p3 punto3 = new p3();
-                punto3.punto3(d);
-                break;
-            case 4:
+        boolean continua = true;
+        while (continua) 
+        {        
+            System.out.println("Ingrese el punto que requiere visualizar del 1 al 10: ");
+            while (!sc.hasNextInt()) //negar la respuestas 
+            {
                 
-                break;
-            case 5:
+                System.out.println("dato incorrecto, ingrese otra vez el numero");
+                sc.next();     
+            }
+            opt = sc.nextInt();
+            int d = m.SolicitarDimension();
+            switch (opt) 
+            {
+                case 1:
+                    p1 punto1 = new p1();
+                    punto1.punto1(d);
+                    break;
+                case 2:
+                    p2 punto2 = new p2();
+                    punto2.punto2(d);
+                    break;
+                case 3:
+                    p3 punto3 = new p3();
+                    punto3.punto3(d);
+                    break;
+                case 4:
                 
-                break;
-            case 6:
+                    break;
+                case 5:
                 
-                break;
-            case 7:
+                    break;
+                case 6:
                 
-                break;
-            case 8:
+                    break;
+                case 7:
                 
-                break;
-            case 9:
+                    break;
+                case 8:
                 
-            break;
+                    break;
+                case 9:
+                
+                    break;
                 case 10:
                 
-                break;
+                    break;
         
-            default:
-                System.out.println("En matenimiento");
-                break;
+                default:
+                    System.out.println("Se ha salido");
+                    continua = false;
+                    break;
+            }
         }
-        
-
-    }
-
-
-    
+    }    
 }
